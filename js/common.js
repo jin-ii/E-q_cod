6807a59788c0ff2b6d41ead8f242ee6cc0ddf890
J <Y>
2024-02-20 16:44:59 +0900

    style : 메인팝업 추가

--- js/common.js
+++ js/common.js
@@ -12,5 +12,21 @@ $(function(){
         $(this).next().stop().slideDown();
 
     });
+
+    //탭메뉴
+    $(".tab_type1 > ul > li > a").click(function(e) {
+
+		var tab_idx = $(".tab_type1 > ul > li > a").index($(this));
+		$(".tab_type1 > ul > li > a").removeClass("on");
+		$(".tab_type1 > ul > li > a").eq(tab_idx).addClass("on");
+		
+		$(".pop_con_list").hide();
+		$(".pop_con_list").eq(tab_idx).show();
+		return false;
+	});
+
+
+
 });
+
 
